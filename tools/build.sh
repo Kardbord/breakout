01bf8cb12932d3eccc995c4b2ae9f38d4542b580
Tanner Kvarfordt <Tanner.Kvarfordt@gmail.com>
2024-10-01 13:28:52 -0700

    Cleaned up build script

--- tools/build.sh
+++ tools/build.sh
@@ -7,7 +7,6 @@ set -o pipefail
 WD="$(dirname "${BASH_SOURCE[0]}")/../build"
 
 FROM_SCRATCH=0
-UNIT_TESTS=0
 CMAKE_ARGS=(-DCMAKE_EXPORT_COMPILE_COMMANDS=ON)
 MAKE_ARGS=()
 
@@ -15,21 +14,19 @@ function usage() {
   echo "${0##*/}"
   echo
   echo "USAGE"
-  echo "${0##*/} [-h] [-s] [-t] [-e] [-m ARG [-m ARG]...] [-c ARG [-c ARG]...]"
+  echo "${0##*/} [-h] [-s] [-m ARG [-m ARG]...] [-c ARG [-c ARG]...]"
   echo
   echo "This is a helper script for building this project."
   echo
   echo "OPTIONS"
   echo "-h -- Prints this usage message."
   echo "-s -- Performs the build from scratch."
-  echo "-t -- Build and run unit tests."
-  echo "-e -- Build examples."
   echo "-m ARG -- Specifies an argument to pass to make (or ninja, if it is available). Can be specified multiple times."
   echo "-c ARG -- Specifies an argument to pass to cmake. Can be specified multiple times."
 }
 
 function process_args() {
-  while getopts ":m:c:hste" opt; do
+  while getopts ":m:c:hs" opt; do
     case "${opt}" in
       m)
         MAKE_ARGS+=("${OPTARG}")
@@ -43,13 +40,6 @@ function process_args() {
       h)
         usage
         exit 0
-        ;;
-      t)
-        UNIT_TESTS=1
-        CMAKE_ARGS+=("-DBUILD_TESTS=ON")
-        ;;
-      e)
-        CMAKE_ARGS+=("-DBUILD_EXAMPLES=ON")
         ;;
       *)
         usage
@@ -82,16 +72,6 @@ function build {
   cmake ${CMAKE_ARGS[*]} ..
   #shellcheck disable=SC2048,2086
   "${builder}" ${MAKE_ARGS[*]}
-  if [[ "${UNIT_TESTS}" = 1 ]]; then
-    for t in ./tests/*_test; do
-      if [[ -x "${t}" ]]; then
-        echo "============================================="
-        echo "Running ${t}"
-        echo "============================================="
-        "${t}"
-      fi
-    done
-  fi
 }
 
 function main {
