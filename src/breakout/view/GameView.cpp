899084c3ad841f8c897b6697927f2cbe7e47cc44
Tanner Kvarfordt <Tanner.Kvarfordt@gmail.com>
2024-10-02 16:13:13 -0700

    Added buttons to main menu

--- src/breakout/view/GameView.cpp
+++ src/breakout/view/GameView.cpp
@@ -1,44 +1,53 @@
-#include <iostream>
 #include <breakout/view/GameView.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_options.hpp>
+#include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <ftxui/dom/node.hpp>
-#include <ftxui/screen/color.hpp>
+#include <iostream>
 
 namespace breakout::view {
 
 auto GameView::render_main_menu(model::GameStateMainMenu const&) -> void {
-  using ftxui::vbox;
-  using ftxui::hbox;
-  using ftxui::text;
-  using ftxui::borderRounded;
-  using ftxui::filler;
-  using ftxui::Screen;
-  using ftxui::Render;
+  using namespace ftxui;
 
-  auto document = vbox({
-    filler(),
-    hbox(
+  auto play_button = Button("  Play  ", []() -> void {}, ButtonOption::Simple());
+  auto quit_button = Button("  Quit  ", []() -> void {}, ButtonOption::Simple());
+
+  auto component = Renderer([&]() -> Element {
+    return vbox({
       filler(),
-      vbox(
-        text(R"(                                                )"),
-        text(R"(    ____                 _               _      )"),
-        text(R"(   | __ ) _ __ ___  __ _| | _____  _   _| |_    )"),
-        text(R"(   |  _ \| '__/ _ \/ _` | |/ / _ \| | | | __|   )"),
-        text(R"(   | |_) | | |  __/ (_| |   < (_) | |_| | |_    )"),
-        text(R"(   |____/|_|  \___|\__,_|_|\_\___/ \__,_|\__|   )"),
-        text(R"(                                                )"),
-        text(R"(                                                )")
-      ) | borderRounded,
-      filler()
-    ),
-    filler(),
-    filler(),
+      hbox({
+        filler(),
+        vbox({
+          text(R"(                                                )"),
+          text(R"(    ____                 _               _      )"),
+          text(R"(   | __ ) _ __ ___  __ _| | _____  _   _| |_    )"),
+          text(R"(   |  _ \| '__/ _ \/ _` | |/ / _ \| | | | __|   )"),
+          text(R"(   | |_) | | |  __/ (_| |   < (_) | |_| | |_    )"),
+          text(R"(   |____/|_|  \___|\__,_|_|\_\___/ \__,_|\__|   )"),
+          text(R"(                                                )"),
+          text(R"(                                                )"),
+        }) | borderRounded,
+        filler(),
+      }),
+      filler(),
+      hbox({
+        filler(),
+        play_button->Render(),
+        filler(),
+      }),
+      filler(),
+      hbox({
+        filler(),
+        quit_button->Render(),
+        filler(),
+      }),
+      filler(),
+    });
   });
 
-  auto screen = Screen::Create(ftxui::Dimension::Full());
-  Render(screen, document);
-  screen.Print();
+  auto screen = ScreenInteractive::Fullscreen();
+  screen.Loop(component);
 }
 
 auto GameView::render_pause_menu(model::GameStatePauseMenu const&)-> void {
