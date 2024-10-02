8c3a9bc3ff80b54ed81b7f208ca1b8f45073a3cc
Tanner Kvarfordt <Tanner.Kvarfordt@gmail.com>
2024-10-02 15:00:39 -0700

    Render title text

--- src/breakout/view/GameView.cpp
+++ src/breakout/view/GameView.cpp
@@ -1,10 +1,44 @@
-#include <breakout/view/GameView.hpp>
 #include <iostream>
+#include <breakout/view/GameView.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/node.hpp>
+#include <ftxui/screen/color.hpp>
 
 namespace breakout::view {
 
 auto GameView::render_main_menu(model::GameStateMainMenu const&) -> void {
-  std::cout << "Rendering main menu!\n";
+  using ftxui::vbox;
+  using ftxui::hbox;
+  using ftxui::text;
+  using ftxui::borderRounded;
+  using ftxui::filler;
+  using ftxui::Screen;
+  using ftxui::Render;
+
+  auto document = vbox({
+    filler(),
+    hbox(
+      filler(),
+      vbox(
+        text(R"(                                                )"),
+        text(R"(    ____                 _               _      )"),
+        text(R"(   | __ ) _ __ ___  __ _| | _____  _   _| |_    )"),
+        text(R"(   |  _ \| '__/ _ \/ _` | |/ / _ \| | | | __|   )"),
+        text(R"(   | |_) | | |  __/ (_| |   < (_) | |_| | |_    )"),
+        text(R"(   |____/|_|  \___|\__,_|_|\_\___/ \__,_|\__|   )"),
+        text(R"(                                                )"),
+        text(R"(                                                )")
+      ) | borderRounded,
+      filler()
+    ),
+    filler(),
+    filler(),
+  });
+
+  auto screen = Screen::Create(ftxui::Dimension::Full());
+  Render(screen, document);
+  screen.Print();
 }
 
 auto GameView::render_pause_menu(model::GameStatePauseMenu const&)-> void {
